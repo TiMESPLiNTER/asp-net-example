6f8d0cbc4596d6093c6e55db43b491cd9f44fa31
TiMESPLiNTER <github@timesplinter.ch>
2019-08-22 20:18:57 +0200

    Initial commit

---
+++ BMI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class Bmi
+    {
+        public long Weight { get; set; }
+
+        public long Height { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public long Weight { get; set; }
+
+        public long Height { get; set; }
+
+        public Bmi Bmi { get; set; } = new Bmi(){Height = 170, Weight = 80}; // Startdata
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost(long Height, long Weight)
+        {
+            this.Bmi.Height = this.Height = Height;
+            this.Bmi.Weight = this.Weight = Weight;
+        }
+    }
+}
